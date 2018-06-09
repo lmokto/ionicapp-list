723320b60ba47a9f604637bc7b77f94861798358
lmokto <lm.uranga@gmail.com>
2018-06-09 13:47:34 +0200

    retiro components y creo nuevos

---
+++ src/pages/pendientes/pendientes.components.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pendientes',
+  templateUrl: 'pendientes.component.html'
+})
+export class PendientesComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {}
+
+}
---
+++ src/pages/terminados/terminados.components.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terminados',
+  templateUrl: 'terminados.component.html'
+})
+export class TerminadosComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {}
+
+}
